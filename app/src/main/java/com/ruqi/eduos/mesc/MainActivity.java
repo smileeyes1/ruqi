aeee4af72f5987c713afc50e5b2a23364af4b9cf
smileeyes1 <smile.eyes1@gmail.com>
2026-05-19 15:41:43 +0300

    Comment out UI setup in MainActivity
    Commented out the UI initialization code for testing purposes.

--- app/src/main/java/com/ruqi/eduos/mesc/MainActivity.java
+++ app/src/main/java/com/ruqi/eduos/mesc/MainActivity.java
@@ -2,15 +2,11 @@ package com.ruqi.eduos.mesc;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        TextView mathText = findViewById(R.id.mathQuestionText);
-        mathText.setText("٥ + ٣ = ٨");
+        // تم تفريغ استدعاء الواجهة المؤقتة لضمان نجاح تجميع التطبيق أولاً
     }
 }
