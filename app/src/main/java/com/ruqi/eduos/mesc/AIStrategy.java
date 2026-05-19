89859d8abf82cd6a6987ef3b631f7600454ddcf7
smileeyes1 <smile.eyes1@gmail.com>
2026-05-19 22:00:24 +0300

    Remove comment about variable checks in AIStrategy
    Removed commented line about variable checks.

--- app/src/main/java/com/ruqi/eduos/mesc/AIStrategy.java
+++ app/src/main/java/com/ruqi/eduos/mesc/AIStrategy.java
@@ -4,6 +4,5 @@ import android.content.Context;
 
 public interface AIStrategy {
     boolean isAvailable(Context context);
-    // تأكد من وجود المتغيرات كاملة هنا
     void execute(Context context, String prompt, AIProcessor.Callback callback);
 }
