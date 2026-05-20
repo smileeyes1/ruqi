0c1552595d10ecb681d311537fdbdc21784a7a20
smileeyes1 <smile.eyes1@gmail.com>
2026-05-20 05:44:08 +0300

    Delete app/src/main/java/com/ruqi/eduos/mesc/AppCommand.java

--- app/src/main/java/com/ruqi/eduos/mesc/AppCommand.java
+++
@@ -1,8 +0,0 @@
-package com.ruqi.eduos.mesc;
-
-import android.content.Context;
-
-public interface AppCommand {
-    String getName();
-    void execute(Context context, String input, AIProcessor.Callback callback);
-}
