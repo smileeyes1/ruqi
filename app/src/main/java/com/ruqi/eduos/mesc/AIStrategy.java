29df3258c62ef524af3817db4a1e8541737584f1
smileeyes1 <smile.eyes1@gmail.com>
2026-05-20 05:42:01 +0300

    Delete app/src/main/java/com/ruqi/eduos/mesc/AIStrategy.java

--- app/src/main/java/com/ruqi/eduos/mesc/AIStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.ruqi.eduos.mesc;
-
-import android.content.Context;
-
-public interface AIStrategy {
-    boolean isAvailable(Context context);
-    void execute(Context context, String prompt, AIProcessor.Callback callback);
-}
