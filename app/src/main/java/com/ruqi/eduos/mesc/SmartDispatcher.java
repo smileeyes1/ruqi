79a0d0f91bc6f64e6cb176ccc326e7ea9151e017
smileeyes1 <smile.eyes1@gmail.com>
2026-05-20 05:35:15 +0300

    Delete app/src/main/java/com/ruqi/eduos/mesc/SmartDispatcher.java

--- app/src/main/java/com/ruqi/eduos/mesc/SmartDispatcher.java
+++
@@ -1,9 +0,0 @@
-package com.ruqi.eduos.mesc;
-
-import android.content.Context;
-
-public class SmartDispatcher {
-    public static void dispatch(Context context, String userInput, AIProcessor.Callback callback) {
-        AIOrchestrator.process(context, userInput, callback);
-    }
-}
